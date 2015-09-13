967b47758e309fa59ad4fc35da072bb1196bda9b
pavetok <p.vetokhin@gmail.com>
2015-09-13 15:11:49 +0300

    yet another tests

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -18,17 +18,14 @@ def mix(cases):
 def test_get_one(cases, mix):
     # given
     username = mix(5)
-    gender = 'female'
     age = 17
     # when
     case = cases.get_one(
         username=username,
-        gender=gender,
         age=age
     )
     # then
     assert case.username == username
-    assert case.gender == gender
     assert case.age == age
 
 
@@ -44,16 +41,71 @@ def test_each_choice(cases, mix):
         age=(age1, age2, age3)
     )
     # and
-    cases = list(cases)
+    actuals = list(cases)
     # then
-    assert cases[0].username == username1
-    assert cases[0].gender == gender
-    assert cases[0].age == age1
+    assert actuals[0].username == username1
+    assert actuals[0].gender == gender
+    assert actuals[0].age == age1
     # and
-    assert cases[1].username == username2
-    assert cases[1].gender == gender
-    assert cases[1].age == age2
+    assert actuals[1].username == username2
+    assert actuals[1].gender == gender
+    assert actuals[1].age == age2
     # and
-    assert cases[2].username == username1
-    assert cases[2].gender == gender
-    assert cases[2].age == age3
+    assert actuals[2].username == username1
+    assert actuals[2].gender == gender
+    assert actuals[2].age == age3
+
+
+def test_pairwise(cases, mix):
+    # given
+    username1, username2 = (mix(1), mix(64))
+    age1, age2, age3 = (1, 17, 18)
+    # when
+    cases = cases.get_pairwise(
+        username=(username1, username2),
+        age=(age1, age2, age3)
+    )
+    # and
+    actuals = list(cases)
+    # then
+    assert len(actuals) == 6
+    # and
+    assert actuals[0].username == username1
+    assert actuals[0].age == age1
+    # and
+    assert actuals[1].username == username2
+    assert actuals[1].age == age1
+    # and
+    assert actuals[2].username == username2
+    assert actuals[2].age == age2
+    # and
+    assert actuals[3].username == username1
+    assert actuals[3].age == age2
+    # and
+    assert actuals[4].username == username1
+    assert actuals[4].age == age3
+    # and
+    assert actuals[5].username == username2
+    assert actuals[5].age == age3
+
+
+def test_negative(cases, mix):
+    # given
+    invalid_username, valid_username = (mix(65), mix(10))
+    invalid_age1, invalid_age2, valid_age = (0, 1000, 10)
+    # when
+    cases = cases.get_negative(
+        username=(invalid_username, {'default': valid_username}),
+        age=(invalid_age1, invalid_age2, {'default': valid_age})
+    )
+    # and
+    actuals = list(cases)
+    # then
+    assert actuals[0].username == invalid_username
+    assert actuals[0].age == valid_age
+    # and
+    assert actuals[1].username == valid_username
+    assert actuals[1].age == invalid_age1
+    # and
+    assert actuals[2].username == valid_username
+    assert actuals[2].age == invalid_age2
